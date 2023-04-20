4addef3074de44feda20abee6959ee99dcb7687d
Chris Risner <crisner.prsvr@gmail.com>
2023-04-19 23:50:36 -0500

    restructured selectMenu, more consistent with other inputs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
       <div className='flex items-center space-x-[6px]'>
         <Image src='/PoweredByDark.svg' alt='Powered by' width={117} height={22} />
       </div>
-      <div className='h-[20px] w-px bg-[#8792A2]' />
+      <div className='h-[20px] w-px bg-[#8792A2]/30' />
       <div className='flex space-x-6 font-sans text-[12px] leading-[22px] font-light tracking-[0.015em] text-[#8792A2]'>
         <a href='https://driv.ly/terms'>Terms</a>
         <a href='https://driv.ly/privacy'>Privacy</a>
--- src/components/SelectMenu.tsx
+++ src/components/SelectMenu.tsx
@@ -18,76 +18,83 @@ export default function SelectField(props: any) {
   console.log('method.field.value', method.field.value)
   return (
     <div className={`${variant} relative`}>
-      <label
-        className={`${
-          errormsg ? 'text-red-400' : 'text-gray-900'
-        } block text-sm font-medium leading-6 `}
-        htmlFor={name}>
-        {label}
-      </label>
       <Listbox
         {...method.field}
         value={method.field.value ? method.field.value : cats[0].optionName}
         name={name}
         as='div'
-        className='mt-2'>
+        className=''>
         {({ open }) => (
-          <div className=' h-full w-full'>
-            <Listbox.Button
+          <>
+            <Listbox.Label
               className={`${
-                errormsg
-                  ? 'text-red-400 outline-none ring-1 ring-inset ring-red-400 focus:ring-2 focus:ring-inset focus:ring-red-400'
-                  : 'outline-none ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-DRIVLY'
-              } block h-[36px] w-full rounded-md border-0 px-3 text-left text-gray-900 shadow-sm ring-1 ring-inset  sm:max-w-xs sm:text-sm sm:leading-6`}>
-              <div className='flex h-full w-full cursor-pointer items-center justify-between'>
-                <p
-                  className={`${
-                    method.field.value !== undefined ? 'text-gray-900' : 'text-[#8E8EA3]/50'
-                  }  w-full flex-1 text-sm font-normal`}>
-                  {method.field.value ? method.field.value : cats[0].optionName}
-                </p>
-                <ChevronDownIcon className='h-5 w-5 text-skin-base/50' />
-              </div>
-            </Listbox.Button>
-            {open && (
-              <Listbox.Options className='absolute left-0 z-10 mt-1 h-56 w-full overflow-auto rounded-md border border-BORDER_DARK bg-stone-600 py-1 scrollbar-hide'>
-                {cats.map((cat: any, i: number) => (
-                  <Listbox.Option
-                    key={i}
-                    value={cat.value}
-                    className={({ active }) =>
-                      `relative mx-1 cursor-default select-none py-2 pl-6 pr-4 text-xs ${
-                        active ? 'bg-blue-400 text-white' : 'text-gray-200'
-                      }`
-                    }>
-                    {({ selected }) => (
-                      <>
-                        <span
-                          className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
-                          {cat.value}
-                        </span>
-                        {selected ? (
-                          <span className='absolute inset-y-0 left-0 flex items-center pl-1.5 text-white'>
-                            <CheckIcon className='h-3 w-4' aria-hidden='true' />
+                errormsg ? 'text-red-400' : 'text-gray-900'
+              } block text-sm font-medium leading-6 `}
+              htmlFor={name}>
+              {label}
+            </Listbox.Label>
+
+            <div className='relative mt-2 h-full w-full'>
+              <Listbox.Button
+                className={`${
+                  errormsg
+                    ? 'text-red-400 outline-none ring-1 ring-inset ring-red-400 focus:ring-2 focus:ring-inset focus:ring-red-400'
+                    : 'outline-none ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-DRIVLY'
+                } block h-[36px] w-full rounded-md border-0 px-3 text-left text-gray-900 shadow-sm ring-1 ring-inset  sm:max-w-xs sm:text-sm sm:leading-6`}>
+                <div className='flex h-full w-full cursor-pointer items-center justify-between'>
+                  <span
+                    className={`${
+                      method.field.value !== undefined ? 'text-gray-900' : 'text-[#8E8EA3]/50'
+                    }  w-full flex-1 text-sm font-normal`}>
+                    {method.field.value ? method.field.value : cats[0].optionName}
+                  </span>
+                  <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
+                    <ChevronDownIcon className='h-5 w-5 text-skin-base/50' aria-hidden='true' />
+                  </span>
+                </div>
+              </Listbox.Button>
+              {open && (
+                <Listbox.Options className='absolute left-0 z-10 mt-1 h-48 w-full overflow-auto rounded-md border border-BORDER_DARK bg-stone-600 py-1 scrollbar-hide'>
+                  {cats.map((cat: any, i: number) => (
+                    <Listbox.Option
+                      key={i}
+                      value={cat.value}
+                      className={({ active }) =>
+                        `relative mx-1 cursor-default select-none py-2 pl-6 pr-4 text-xs ${
+                          active ? 'bg-blue-400 text-white' : 'text-gray-200'
+                        }`
+                      }>
+                      {({ selected }) => (
+                        <>
+                          <span
+                            className={`block truncate ${
+                              selected ? 'font-medium' : 'font-normal'
+                            }`}>
+                            {cat.value}
                           </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
+                          {selected ? (
+                            <span className='absolute inset-y-0 left-0 flex items-center pl-1.5 text-white'>
+                              <CheckIcon className='h-3 w-4' aria-hidden='true' />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
+                </Listbox.Options>
+              )}
+            </div>
+            {errormsg && (
+              <span
+                className={`${
+                  !label ? '-top-2' : 'top-4'
+                } absolute  right-0 text-[11px] font-medium text-red-400`}>
+                {errormsg?.toString()}
+              </span>
             )}
-          </div>
+          </>
         )}
       </Listbox>
-      {errormsg && (
-        <span
-          className={`${
-            !label ? '-top-2' : 'top-4'
-          } absolute  right-0 text-[11px] font-medium text-red-400`}>
-          {errormsg?.toString()}
-        </span>
-      )}
     </div>
   )
 }
