2bff1f5a43d11eb45b8c448ff7a980693d407444
Chris Risner <crisner.prsvr@gmail.com>
2023-04-24 11:50:54 -0500

    edit back

--- src/components/Backarrow.tsx
+++ src/components/Backarrow.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { ChevronLeftIcon } from '@heroicons/react/20/solid'
 
 type BackarrowProps = {
@@ -9,7 +11,7 @@ const Backarrow = ({ cancelUrl, variants }: BackarrowProps) => {
   return (
     <a
       href={cancelUrl}
-      className={`${variants} absolute z-50 grid h-12 w-12 place-content-center place-items-center rounded-[5px] border-none bg-[#8792A2]/10 outline-none ring-0 drop-shadow-sm active:outline-none `}>
+      className={`${variants && variants} absolute z-50 grid h-12 w-12 place-content-center place-items-center rounded-[5px] border-none bg-[#8792A2]/10 outline-none ring-0 drop-shadow-sm active:outline-none `}>
       <ChevronLeftIcon className='h-6 w-6 text-DETAIL__BLACK' />
     </a>
   )
