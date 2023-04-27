9bd170720e653cd520bdef0a6540dd49daf82900
Chris Risner <crisner.prsvr@gmail.com>
2023-04-27 08:47:23 -0500

    update backarrow

--- src/components/Backarrow.tsx
+++ src/components/Backarrow.tsx
@@ -13,7 +13,7 @@ const Backarrow = () => {
     <button
       onClick={() => router.back()}
       className={`${
-        isHome ? '-top-5 left-4 my-2 sm:left-8' : 'left-4 top-4 sm:left-8 lg:top-[88px]'
+        isHome ? '-top-5 left-4 my-2 sm:left-8' : 'left-4 top-4 sm:left-8 lg:top-16'
       } absolute z-50 grid h-8 w-8 place-content-center place-items-center rounded-[5px] border-none outline-none ring-0 drop-shadow-sm hover:bg-[#8792A2]/10 active:outline-none`}>
       <ChevronLeftIcon className='h-7 w-7 text-DETAIL__BLACK' />
     </button>
