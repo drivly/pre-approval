caca9c824dd1d77bb69820517e4304bd7711cc48
Chris Risner <crisner.prsvr@gmail.com>
2023-07-07 12:49:02 -0500

    updated footer color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,19 @@
+import { cn } from '@utils'
 import PoweredByDrivly from './PoweredByDrivly'
 
 export default function Footer({ hasVin }: { hasVin?: any }) {
   return (
     <div
-      className={`${
-        hasVin === true ? 'justify-center space-x-8 lg:justify-between' : 'justify-center space-x-8'
-      } flex h-[38px] w-full items-center `}>
-      <div className='flex items-center space-x-[6px]'>
-        <PoweredByDrivly />
-      </div>
+      className={cn('flex h-[38px] w-full items-center justify-center space-x-8 text-[#A2A2A5]', {
+        'justify-center space-x-8 lg:justify-between ': hasVin,
+      })}>
+      <PoweredByDrivly />
 
-      <div className='font-sans flex items-center space-x-4 text-[12px] font-light leading-[22px] tracking-[0.015em] text-[#8792A2]'>
+      <div className='font-sans flex items-center space-x-4 text-[12px] font-light leading-[22px] tracking-[0.015em]'>
         <a href='https://driv.ly/terms' target='_blank'>
           Terms
         </a>
-        <div className='h-[16px] w-px rotate-12 bg-[#8792A2]/60' />
+        <div className='h-[16px] w-px rotate-12 bg-[#A2A2A5]/60' />
         <a href='https://driv.ly/privacy' target='_blank'>
           Privacy
         </a>
